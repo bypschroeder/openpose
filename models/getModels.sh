18d9bfc5e11038bee69f380365e490e4032af647
bypschroeder <patrick.schroeder@hof-university.de>
2024-12-08 21:41:43 +0100

    edited getModels.sh

--- models/getModels.sh
+++ models/getModels.sh
@@ -9,12 +9,12 @@ HAND_FOLDER="hand/"
 # Body (BODY_25)
 BODY_25_FOLDER=${POSE_FOLDER}"body_25/"
 BODY_25_MODEL=https://github.com/bypschroeder/openpose_models/raw/refs/heads/main/pose_iter_584000.caffemodel?download=
-(cd ${BODY_25_FOLDER} && curl -O ${BODY_25_MODEL})
+(cd ${BODY_25_FOLDER} && curl -L -O ${BODY_25_MODEL})
 
 # Body (COCO)
 COCO_FOLDER=${POSE_FOLDER}"coco/"
 COCO_MODEL=https://github.com/bypschroeder/openpose_models/raw/refs/heads/main/pose_iter_440000.caffemodel?download=
-(cd ${COCO_FOLDER} && curl -O ${COCO_MODEL})
+(cd ${COCO_FOLDER} && curl -L -O ${COCO_MODEL})
 # Alternative: it will not check whether file was fully downloaded
 # if [ ! -f $COCO_MODEL ]; then
 #     wget ${OPENPOSE_URL}$COCO_MODEL -P $COCO_FOLDER
@@ -23,14 +23,14 @@ COCO_MODEL=https://github.com/bypschroeder/openpose_models/raw/refs/heads/main/p
 # Body (MPI)
 MPI_FOLDER=${POSE_FOLDER}"mpi/"
 MPI_MODEL=https://github.com/bypschroeder/openpose_models/raw/refs/heads/main/pose_iter_160000.caffemodel?download=
-(cd ${MPI_FOLDER} && curl -O ${MPI_MODEL})
+(cd ${MPI_FOLDER} && curl -L -O ${MPI_MODEL})
 
 # "------------------------- FACE MODELS -------------------------"
 # Face
 FACE_MODEL=https://github.com/bypschroeder/openpose_models/raw/refs/heads/main/pose_iter_116000.caffemodel?download=
-(cd ${FACE_FOLDER} && curl -O ${FACE_MODEL})
+(cd ${FACE_FOLDER} && curl -L -O ${FACE_MODEL})
 
 # "------------------------- HAND MODELS -------------------------"
 # Hand
 HAND_MODEL=https://github.com/bypschroeder/openpose_models/raw/refs/heads/main/pose_iter_102000.caffemodel?download=
-(cd ${HAND_FOLDER} && curl -O ${HAND_MODEL})
+(cd ${HAND_FOLDER} && curl -L -O ${HAND_MODEL})
