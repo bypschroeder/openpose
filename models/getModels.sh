96d20af03cca8d50aba4fef62dfce0f4e22b3463
bypschroeder <patrick.schroeder@hof-university.de>
2024-12-08 21:15:26 +0100

    edited getModels.sh

--- models/getModels.sh
+++ models/getModels.sh
@@ -8,13 +8,13 @@ HAND_FOLDER="hand/"
 # ------------------------- POSE (BODY+FOOT) MODELS -------------------------
 # Body (BODY_25)
 BODY_25_FOLDER=${POSE_FOLDER}"body_25/"
-BODY_25_MODEL=${BODY_25_FOLDER}"pose_iter_584000.caffemodel"
-wget -c ${OPENPOSE_URL}${BODY_25_MODEL} -P ${BODY_25_FOLDER}
+BODY_25_MODEL=https://github.com/bypschroeder/openpose_models/raw/refs/heads/main/pose_iter_584000.caffemodel?download=
+wget -c ${BODY_25_MODEL} -P ${BODY_25_FOLDER}
 
 # Body (COCO)
 COCO_FOLDER=${POSE_FOLDER}"coco/"
-COCO_MODEL=${COCO_FOLDER}"pose_iter_440000.caffemodel"
-wget -c ${OPENPOSE_URL}${COCO_MODEL} -P ${COCO_FOLDER}
+COCO_MODEL=https://github.com/bypschroeder/openpose_models/raw/refs/heads/main/pose_iter_440000.caffemodel?download=
+wget -c ${COCO_MODEL} -P ${COCO_FOLDER}
 # Alternative: it will not check whether file was fully downloaded
 # if [ ! -f $COCO_MODEL ]; then
 #     wget ${OPENPOSE_URL}$COCO_MODEL -P $COCO_FOLDER
@@ -22,15 +22,15 @@ wget -c ${OPENPOSE_URL}${COCO_MODEL} -P ${COCO_FOLDER}
 
 # Body (MPI)
 MPI_FOLDER=${POSE_FOLDER}"mpi/"
-MPI_MODEL=${MPI_FOLDER}"pose_iter_160000.caffemodel"
-wget -c ${OPENPOSE_URL}${MPI_MODEL} -P ${MPI_FOLDER}
+MPI_MODEL=https://github.com/bypschroeder/openpose_models/raw/refs/heads/main/pose_iter_160000.caffemodel?download=
+wget -c ${MPI_MODEL} -P ${MPI_FOLDER}
 
 # "------------------------- FACE MODELS -------------------------"
 # Face
-FACE_MODEL=${FACE_FOLDER}"pose_iter_116000.caffemodel"
-wget -c ${OPENPOSE_URL}${FACE_MODEL} -P ${FACE_FOLDER}
+FACE_MODEL=https://github.com/bypschroeder/openpose_models/raw/refs/heads/main/pose_iter_116000.caffemodel?download=
+wget -c ${FACE_MODEL} -P ${FACE_FOLDER}
 
 # "------------------------- HAND MODELS -------------------------"
 # Hand
-HAND_MODEL=$HAND_FOLDER"pose_iter_102000.caffemodel"
-wget -c ${OPENPOSE_URL}${HAND_MODEL} -P ${HAND_FOLDER}
+HAND_MODEL=https://github.com/bypschroeder/openpose_models/raw/refs/heads/main/pose_iter_102000.caffemodel?download=
+wget -c ${HAND_MODEL} -P ${HAND_FOLDER}
